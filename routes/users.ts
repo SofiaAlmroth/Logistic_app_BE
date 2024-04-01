dcdc9a3874e27a6c409dfc598f2af5252793087c
Ammar <ammarelkhalil@outlook.com>
2024-04-01 13:00:00 +0200

    added user route

--- routes/users.ts
+++ routes/users.ts
@@ -1,9 +1,30 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { validate } from "../schemas/User";
 
+const prisma = new PrismaClient();
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  return res.send("creating user..");
+router.post("/", async (req, res) => {
+  const validation = validate(req.body);
+
+  if (!validation.success)
+    return res.status(400).send(validation.error.issues[0].message);
+
+  const existingUser = await prisma.user.findFirst({
+    where: { email: req.body.email },
+  });
+
+  if (existingUser) return res.status(400).send("User already exists");
+
+  const user = await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    },
+  });
+  return res.send(user);
 });
 
 export default router;
