43834b800bec60a01b46b3ff8b84a21a5c388ce2
Ammar <ammarelkhalil@outlook.com>
2024-04-04 18:39:35 +0200

    added auth route and schema

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import paintColors from "./routes/paintColors";
 import users from "./routes/users";
+import auth from "./routes/auth";
 import express from "express";
 import cors from "cors";
 
@@ -9,5 +10,6 @@ app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use("/api/paintcolors", paintColors);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 app.listen(5999, () => console.log("listening on port 5999..."));
